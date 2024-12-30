c7e5b3dc5348630db378f74127c09605fdbc7153
Edwin Cardenas <eacardenase@gmail.com>
2024-12-30 12:00:39 -0500

    feat: add pointers manipulation inside function

--- PBR/main.c
+++ PBR/main.c
@@ -8,16 +8,36 @@
 #include <stdio.h>
 #include <math.h>
 
+void metersToFeetAndInches(double meters, unsigned int *ftPtr, double *inPtr)
+{
+    // This function assumes meters is non-negative.
+    
+    // Convert the number of meters into a floating-point number of feet
+    double rawFeet = meters * 3.281;
+    
+    // How many complete feet as an unsigned int?
+    unsigned int feet = (unsigned int)floor(rawFeet);
+    
+    // Store the number of feet at the supplied address
+    *ftPtr = feet;
+    
+    // Calculate inches
+    double fractionalFoot = rawFeet - feet;
+    double inches = fractionalFoot * 12.0;
+    
+    // Store the number of inches at the supplied address
+    printf("Storing %.2f to the address %p\n", inches, inPtr);
+    *inPtr = inches;
+}
+
 int main(int argc, const char * argv[]) {
-    double pi = 3.14;
-    double integerPart;
-    double fractionPart;
+    double meters = 3.0;
+    unsigned int feet;
+    double inches;
     
-    // Pass the address of integerPart as an argument
-    fractionPart = modf(pi, &integerPart);
+    metersToFeetAndInches(meters, &feet, &inches);
     
-    // Find the value stored in integerPart
-    printf("integerPart = %.0f, fractionPart = %.2f\n", integerPart, fractionPart);
+    printf("%.1f meters is equal to %d feet and %.1f inches.\n", meters, feet, inches);
     
     return 0;
 }
